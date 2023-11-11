faccc8ce3718ab8244c694621d48971370f312fd
Saragj95 <sara.guerrouj.22@gmail.com>
2023-11-11 13:18:40 -0800

    commit

--- _int.c
+++ _int.c
@@ -9,7 +9,7 @@
 int _int(va_list args)
 {
 	int i = va_arg(args, int);
-	int x = 0, l = 0, j, len = 0;
+	int x = 0, l = 0, j = 0, len = 0;
 	int *digit;
 	char digitChar;
 
@@ -22,32 +22,30 @@ int _int(va_list args)
 	{
 		write(1, "0", 1);
 		return (1);
-	} else
-	{
-		j = i;
-		while (j != 0)
-		{
-			x = j % 10;
-			j = j / 10;
-			l++;
-		}
-		digit = malloc(l * sizeof(int));
-		if (digit == NULL)
-		{
-			return (-1);
-		}
-		for (x = 0; x < l; x++)
-		{
-			digit[x] = i % 10;
-			i = i / 10;
-		}
-		for (x = (l - 1); x >= 0 ; x--)
-		{
-			digitChar = digit[x] + '0';
-			write(1, &digitChar, 1);
-			len++;
-		}
-		free(digit);
 	}
+	j = i;
+	while (j != 0)
+	{
+		x = j % 10;
+		j = j / 10;
+		l++;
+	}
+	digit = malloc(l * sizeof(int));
+	if (digit == NULL)
+	{
+		return (-1);
+	}
+	for (x = 0; x < l; x++)
+	{
+		digit[x] = i % 10;
+		i = i / 10;
+	}
+	for (x = (l - 1); x >= 0 ; x--)
+	{
+		digitChar = digit[x] + '0';
+		write(1, &digitChar, 1);
+		len++;
+	}
+	free(digit);
 	return (len);
 }
