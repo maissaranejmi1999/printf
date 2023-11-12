c7bfe9f28c0314b4462f094870fe6f35ded3ccaf
Saragj95 <sara.guerrouj.22@gmail.com>
2023-11-11 16:35:20 -0800

    commit

--- main.h
+++ main.h
@@ -1,16 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <limits.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
-int _char(va_list args);
-int _string(va_list args);
 int _int(va_list args);
+int _string(va_list args);
+int _char(va_list args);
 int _putchar(char c);
 
 #endif
