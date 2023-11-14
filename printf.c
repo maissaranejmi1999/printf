090aa1604b7376a57104e2dd40bd9bfed573d32b
maissaranejmi1999 <molinmay1999@gmail.com>
2023-11-14 15:52:03 -0800

    m

--- printf.c
+++ printf.c
@@ -7,61 +7,44 @@
 */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int i = 0;
-        const char *p;
+	va_list args;
+	int i = 0;
+	const char *p;
 
-        va_start(args, format);
-        if (format == NULL)
-        {
-                return (-1);
-        }
-        for (p = format; *p != '\0'; p++)
-        {
-                if (*p == '%')
-                {
-                        if (*(p + 1) == 'c')
-                        {
-                                i += _char(args);
-                        } else if (*(p + 1) == 's')
-                        {
-                                i += _string(args);
-                        } else if ((*(p + 1) == 'd') || (*(p + 1) == 'i'))
-                        {
-                                i += _int(args);
-                        } else if (*(p + 1) == '%')
-                        {
-                                write(1, "%", 1);
-                                i++;
-                        } else
-                        {
-                                va_end(args);
-                                return (-1);
-                        }
-                        p++;
-                } else if (*p == '\\')
-                {
-                        if (*(p + 1) == 'n')
-                        {
-                                write(1, "\n", 1);
-                                i++;
-                        } else if (*(p + 1) == '\\')
-                        {
-                                write(1, "\\", 1);
-                                i++;
-                        } else
-                        {
-                                va_end(args);
-                                return (-1);
-                        }
-                        p++;
-                } else
-                {
-                        write(1, &(*p), 1);
-                        i++;
-                }
-        }
-        va_end(args);
-        return (i);
+	va_start(args, format);
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	for (p = format; *p != '\0'; p++)
+	{
+		if (*p == '%')
+		{
+			if (*(p + 1) == 'c')
+			{
+				i += _char(args);
+			} else if (*(p + 1) == 's')
+			{
+				i += _string(args);
+			} else if ((*(p + 1) == 'd') || (*(p + 1) == 'i'))
+			{
+				i += _int(args);
+			} else if (*(p + 1) == '%')
+			{
+				write(1, "%", 1);
+				i++;
+			} else
+			{
+				va_end(args);
+				return (-1);
+			}
+			p++;
+		} else
+		{
+			write(1, &(*p), 1);
+			i++;
+		}
+	}
+	va_end(args);
+	return (i);
 }
-
